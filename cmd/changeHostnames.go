c0d78d4ccff62afc5c051e24c17e22bef05521fa
berk131 <berk131@gmail.com>
2022-11-30 09:42:07 +0500

    resume with ishell

--- cmd/changeHostnames.go
+++ cmd/changeHostnames.go
@@ -1,7 +1,48 @@
 package main
 
-import "fmt"
+import (
+	"io/ioutil"
+	"os"
+
+	"golang.org/x/crypto/ssh"
+)
+
+func makeSigner(keyname string) (signer ssh.Signer, err error) {
+	fp, err := os.Open(keyname)
+	if err != nil {
+		return
+	}
+	defer fp.Close()
+
+	buf, _ := ioutil.ReadAll(fp)
+	signer, _ = ssh.ParsePrivateKey(buf)
+	return
+}
+
+func makeKeyring() ssh.ClientAuth {
+	signers := []ssh.Signer{}
+	keys := []string{os.Getenv("HOME") + "/.ssh/id_rsa", os.Getenv("HOME") + "/.ssh/id_dsa"}
+
+	for _, keyname := range keys {
+		signer, err := makeSigner(keyname)
+		if err == nil {
+			signers = append(signers, signer)
+		}
+	}
+
+	return ssh.ClientAuthKeyring(&SignerContainer{signers})
+}
 
 func main() {
-	fmt.Println("vim-go")
+
+	hostname := "10.168.0.1"
+	config := &ssh.ClientConfig{
+		User: "pojos",
+		Auth: []ssh.AuthMethod{makeKeyring()},
+	}
+
+	conn, _ := ssh.Dial("tcp", hostname+":22", config)
+	session, _ := conn.NewSession()
+	defer session.Close()
+
 }
